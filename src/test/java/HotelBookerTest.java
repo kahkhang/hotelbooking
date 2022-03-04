99a6f18d159fd93c6474a634a8f60e6fc628854a
Andrew Low <andrew.low@ninjavan.co>
2022-03-04 19:34:35 +0800

    initial java setup

---
+++ src/test/java/HotelBookerTest.java
@@ -0,0 +1,15 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class HotelBookerTest {
+
+    @Test
+    void add() {
+    }
+
+    @Test
+    void multiply() {
+    }
+
+}
